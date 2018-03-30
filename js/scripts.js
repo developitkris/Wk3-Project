ddad3a5ab0883b7f5538ace4fa13efae268431d8
juni <yyoona86@gmail.com>
2018-03-30 14:48:07 -0700

    add condition for when input is not a number, add empty arr to push into, add styles, other misc fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
 //Business Logic
-var numArray= [];
+var inputNumArr = [];
 
 function beepBooped(number){
-  for (var i=number, i=0; i--){
-
-    if (number.includes(0)){
-      while ((!number.includes(1))||(!number%3 ==0)){
-      number="Beep";
+  for (var i=number; i=0; i--){
+    inputNumArr.push(i);
+      if (number.includes(0)){
+        while ((!number.includes(1))||(!number%3 ==0)){
+        number="Beep";
+        }
+      } else if (number.includes(1)){
+        while (!number%3 ==0){
+        number="Boop";
+        }
+      } else if (number%3 == 0){
+        number="I'm sorry, Dave. I'm afraid I can't do that.";
+      } else {
+        count--;
       }
-    } else if (number.includes(1)){
-      while (!number%3 ==0){
-      number="Boop";
-      }
-    } else if (number%3 == 0){
-      number="I'm sorry, Dave. I'm afraid I can't do that.";
-    } else {
-      count--;
-    }
   }
 }
-
-
 
 
 //User Logic
@@ -28,9 +26,20 @@ $(document).ready(function(){
   $("form#beepBoop").submit(function(event){
     event.preventDefault();
 
-    var userNum = parseInt($("input#inputNum").val());
-    var userResult = beepBooped(userNum);
+    //checks if the input is a number
+    var inputNum = function (){
+      input = parseInt($("input#inputNum").val());
+      if (typeOf(input) == "number" && input>0){
+        input+=input;
+      } else {
+        alert("Please input a number. Any non-numeric values are considered invalid inputs.")
+        reset();
+      };
+    };
 
-    $("#result#" + userResult).show();
-  })
-})
+
+    var userResult = beepBooped(inputNum);
+
+    $("#" + userResult).show();
+  });
+});
